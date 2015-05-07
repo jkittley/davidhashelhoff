292abe4985ca7fa6fcccf247ef3044b48828256f
Mike Jewell <mjewell@gmail.com>
2015-05-07 04:58:14 +0000

    Ring my bell

--- servo.py
+++ servo.py
@@ -1,28 +1,30 @@
-from Tkinter import *
 import RPi.GPIO as GPIO
 import time
 
+RIGHT = 0.8 
+LEFT = 1.6 
+CENTER = 1.2 
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
-pwm = GPIO.PWM(18, 100)
-pwm.start(5)
+pwm = GPIO.PWM(18, 50)
+pwm.start(0)
 
-class App:
+def update(ms):
+  duty = ((ms/1000.0)*50)*100
+  pwm.ChangeDutyCycle(duty)
 
-    def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
-        scale = Scale(frame, from_=0, to=180,
-              orient=HORIZONTAL, command=self.update)
-        scale.grid(row=0)
+def ring_bell():
+  update(RIGHT)
+  time.sleep(2)
+  update(LEFT)
+  time.sleep(0.35)
+  update(RIGHT)
+  time.sleep(0.5)
+  update(CENTER)
+  time.sleep(1)
 
+ring_bell()
 
-    def update(self, angle):
-        duty = float(angle) / 10.0 + 2.5
-        pwm.ChangeDutyCycle(duty)
-
-root = Tk()
-root.wm_title('Servo Control')
-app = App(root)
-root.geometry("200x50+0+0")
-root.mainloop()
+pwm.stop()
+GPIO.cleanup()
