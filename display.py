92d79212b01ffec90e2522bc05476e26b3ef954f
jkittley <jkd3g11@soton.ac.uk>
2015-11-18 13:02:50 +0000

    Swithced to using FBI

--- display.py
+++
@@ -1,6 +0,0 @@
-from Tkinter import *
-root = Tk()
-canvas = Canvas(root, width =1224,height=1000)
-logo=PhotoImage(file="img.jpg")
-# canvas.create_image(0, 0, image=logo) #Change 0, 0 to whichever coordinates you need
-root.mainloop()
