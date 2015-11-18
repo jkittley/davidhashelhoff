777232adc4b905b5dfa580ba1e26ce6b4d89d74f
Mike Jewell <mjewell@gmail.com>
2015-11-18 18:43:30 +0000

    Comments

--- get_word.py
+++ get_word.py
@@ -4,9 +4,7 @@ import urllib
 import json
 import re
 import operator
-url = "http://www.dailymail.co.uk/api/latest_headlines/home/0.json"
-response = urllib.urlopen(url)
-data = json.loads(response.read())
+
 common_words = []
 common = open('common_words.txt', 'r')
 for word in common:
@@ -14,43 +12,79 @@ for word in common:
 
 all_words = {}
 hints = {}
+word_comments = {}
 
-for article in data:
-	large_text = article['largePreviewText']
-	original = large_text
-	large_text = re.sub(r"'", ' ', large_text)
-	result = re.sub(r'[^a-zA-Z\s]', '', large_text)
-	for word in re.split('\s+', result):
-		if len(word) < 5:
+def load_articles(offset):
+	url = "http://www.dailymail.co.uk/api/latest_headlines/home/%d.json" % (offset,)
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+	return data
+
+def load_comments(article_id):
+	response = urllib.urlopen('http://www.dailymail.co.uk/reader-comments/p/asset/readcomments/'+str(article_id))
+	data = json.loads(response.read())
+	comments = []
+	for comment in data['payload']['page']:
+		comments.append(comment['message'].strip())
+	return comments
+
+articles_added = 0
+offset = 0
+while articles_added < 200:
+	data = load_articles(offset)
+	for article in data:
+		if article['channel'] != 'tvshowbiz' and article['channel'] != 'femail':
 			continue
-		if word.lower() == word:
-			continue
-		if word.lower() in common_words:
-			continue
-		found = False
-		for common_word in common_words:
-			if len(common_word) > 3 and word.startswith(common_word):
-				found = True
-		if found:
-			continue
-		if not word in all_words:
-			all_words[word] = 0
-			hints[word] = []
-		
-		all_words[word] += 1
-		sentences = original.split(". ")
-		sentences.append(article['socialHeadline'])
-		for sentence in sentences:
-			sentence = sentence.replace(word,"???").strip()
-			if len(sentence) < 120 and len(sentence) > 20 and not sentence in hints[word]:
-				hints[word].append(sentence)
+		article_id = article['articleId']
+		print article_id, article['socialHeadline']
+		comments = []
+		if int(article['readerCommentsCount']) > 0:
+			comments = load_comments(article_id)
+		large_text = article['largePreviewText']
+		original = large_text
+		large_text = re.sub(r"'", ' ', large_text)
+		result = re.sub(r'[^a-zA-Z\s]', '', large_text)
+		for word in re.split('\s+', result):
+			if len(word) < 5:
+				continue
+			if word.lower() == word:
+				continue
+			if word.lower() in common_words:
+				continue
+			found = False
+			for common_word in common_words:
+				if len(common_word) > 3 and word.startswith(common_word):
+					found = True
+			if found:
+				continue
+			if not word in all_words:
+				all_words[word] = 0
+				hints[word] = []
+				word_comments[word] = []
+			
+			all_words[word] += 1
+			for comment in comments:
+				comment = comment.replace(word,"???").strip()
+				if len(sentence) < 120 and len(comment) > 20 and not comment in word_comments[word]:
+					word_comments[word].append(comment)
+			sentences = re.split('\.\s+', original)
+			sentences.append(article['socialHeadline'])
+			for sentence in sentences:
+				sentence = sentence.replace(word,"???").strip()
+				if len(sentence) < 120 and len(sentence) > 20 and not sentence in hints[word]:
+					hints[word].append(sentence)
+		articles_added += 1
+	offset += len(data)
 
 sorted_x = sorted(all_words.items(), key=operator.itemgetter(1))
 
 for w,a in sorted_x:
-	print "Count:",a
-	print "Hints:"
-	for hint in hints[w]:
-		print "->",hint
-	print "Answer:",w
-	print ""
+	if len(word_comments[w]) > 0:
+		print "Count:",a
+		print "Hints:"
+		for hint in hints[w]:
+			print "Article ->",hint
+		for comment in word_comments[w]:
+			print "Comment ->",comment
+		print "Answer:",w
+		print ""
