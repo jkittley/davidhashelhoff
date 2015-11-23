4c69dc0fa809a5efe86792b3bf09bdd137583841
Mike Jewell <mjewell@gmail.com>
2015-11-23 18:18:22 +0000

    Shifting to nicer headline API

--- get_word.py
+++ get_word.py
@@ -4,15 +4,11 @@ import urllib
 import json
 import re
 import operator
+import sys
 
-common_words = []
-common = open('common_words.txt', 'r')
-for word in common:
-	common_words.append(word.rstrip())
-
-all_words = {}
-hints = {}
-word_comments = {}
+def find_substitutes(text):
+	pieces = re.findall(r'(?P<name>[A-Z][a-z]+\s[A-Z][a-z]+)([\'\s]+[^A-Z]+|$|[\.!,])', text)
+	return [piece[0] for piece in pieces]
 
 def load_articles(offset):
 	url = "http://www.dailymail.co.uk/api/latest_headlines/home/%d.json" % (offset,)
@@ -28,63 +24,24 @@ def load_comments(article_id):
 		comments.append(comment['message'].strip())
 	return comments
 
-articles_added = 0
-offset = 0
-while articles_added < 200:
-	data = load_articles(offset)
-	for article in data:
-		if article['channel'] != 'tvshowbiz' and article['channel'] != 'femail':
-			continue
-		article_id = article['articleId']
-		print article_id, article['socialHeadline']
-		comments = []
-		if int(article['readerCommentsCount']) > 0:
-			comments = load_comments(article_id)
-		large_text = article['largePreviewText']
-		original = large_text
-		large_text = re.sub(r"'", ' ', large_text)
-		result = re.sub(r'[^a-zA-Z\s]', '', large_text)
-		for word in re.split('\s+', result):
-			if len(word) < 5:
+def get_headline_options(n):
+	articles_added = 0
+	offset = 0
+	options = []
+	while articles_added < n:
+		data = load_articles(offset)
+		for article in data:
+			if article['channel'] != 'tvshowbiz' and article['channel'] != 'femail':
 				continue
-			if word.lower() == word:
+			article_id = article['articleId']
+			headline = article['socialHeadline']
+			pieces = find_substitutes(headline)
+			if len(pieces) == 0:
 				continue
-			if word.lower() in common_words:
-				continue
-			found = False
-			for common_word in common_words:
-				if len(common_word) > 3 and word.startswith(common_word):
-					found = True
-			if found:
-				continue
-			if not word in all_words:
-				all_words[word] = 0
-				hints[word] = []
-				word_comments[word] = []
-			
-			all_words[word] += 1
-			for comment in comments:
-				comment = comment.replace(word,"???").strip()
-				if len(sentence) < 120 and len(comment) > 20 and not comment in word_comments[word]:
-					word_comments[word].append(comment)
-			sentences = re.split('\.\s+', original)
-			sentences.append(article['socialHeadline'])
-			for sentence in sentences:
-				sentence = sentence.replace(word,"???").strip()
-				if len(sentence) < 120 and len(sentence) > 20 and not sentence in hints[word]:
-					hints[word].append(sentence)
-		articles_added += 1
-	offset += len(data)
+			for piece in pieces:
+				options.append((headline.replace(piece,'The Hoff'), piece))
+			articles_added += 1
+		offset += len(data)
+	return options
 
-sorted_x = sorted(all_words.items(), key=operator.itemgetter(1))
 
-for w,a in sorted_x:
-	if len(word_comments[w]) > 0:
-		print "Count:",a
-		print "Hints:"
-		for hint in hints[w]:
-			print "Article ->",hint
-		for comment in word_comments[w]:
-			print "Comment ->",comment
-		print "Answer:",w
-		print ""
