dab7f2bddd22327b106bc60dc8cda8fb6969756d
jkittley <jkd3g11@soton.ac.uk>
2015-11-24 19:33:07 +0000

    Added strings for game answers and questions

---
+++ strings.py
@@ -0,0 +1,60 @@
+STRINGS = {
+    'wrong_answer': [
+        "Nope {{ PLAYER }}",
+        "Ha that was a rubish guess {{ PLAYER }}",
+        "My pet ostrich could have done better {{ PLAYER }}",
+        "{{ PLAYER }} Pathetic",
+        "Hmm pretty good {{ PLAYER }}... for a losser!",
+        "Let me think {{ PLAYER }}... No!",
+        "At least you tried {{ PLAYER }}",
+        "{{ PLAYER }} Whoooooooooooooooooooooo! I loved mark and lard.",
+        "You should have gone to Specsavers {{ PLAYER }}",
+        "Well done! For managing to type with your sausage fingers {{ PLAYER }}.",
+        "{{ PLAYER }} Super job sport, but no.",
+        "Smell my finger {{ PLAYER }}!",
+        "SHGJ&£dsff is better than your gusess {{ PLAYER }}"
+    ],
+    'right_answer': [
+        "Boom! {{ WINNER }} guessed it! well done smarty pants.",
+        "Holly Hoff {{ WINNER }} got it right! And I always thought you werent that bright.",
+        "Did you cheat {{ WINNER }}? Oh okay then. "
+        "Hofftastic {{ WINNER }}! You is da champ.",
+        "Nice job ace! Everyone congratulate {{ WINNER }}.",
+        "Give yourself a pat on the back {{ WINNER }}.",
+        "{{ WINNER }} you make my heart sing, you make my bell ding-a-ling, you make everything...",
+        "Ding-Ding we have a winner - {{ WINNER }}",
+        "Ah can you smell it {{ WINNER }}. That's the smell of success.",
+        "KA BOOM! {{ WINNER }} is on fire!",
+        "Hoff Phone Home... and tell them we have a winner! {{ WINNER }}"
+    ],
+    'game_start': [
+        "Let's get ready to name jumble!",
+        "It's time to play everyones favorite game!",
+        "Put down you tea and let's play Who has Hoff Hidden",
+        "Stop everything! It's time to play!",
+        "Holly Hoff is that the time? It's Hoff past game o'clock!"
+    ],
+    'no_winners': [
+        "The answer was {{ ANSWER }}, who would have hoffed it!",
+        "I'm smarter than every one or I have no friends, nope I is the smartest. The answer was {{ ANSWER }}.",
+        "The answer was {{ ANSWER }}. Let's play again tomorrow.",
+        "Hello! Anyone there? The answer was {{ ANSWER }}.",
+        "Try harder chumps. The answer was {{ ANSWER }}."
+    ],
+    'already_over': [
+        "Sorry chap, it's all over. Maybe tomorrow.",
+        "All done for today old bean, try again tomorrow.",
+        "The bell has tolled today, maybe next time.",
+        "Weren't you listening, it's done, over, no more - well for today anyway.",
+        "Sorry but you where too slow. Tomorrow?",
+        "Wow, you are late and not fashionably so.",
+        "Smoke me a kipper I'll be back for breakfast, by which I mean it's over, lets play tomorrow.",
+        "Tumble weed... Town clock chimes: dong, dong. Game over, well for today at least",
+        "Bonus question! What always too late and smells of cabbage? It's you. Come back tomorrow.",
+        "I'll be back... tomorrow for another game.",
+        "Frankly, my dear, I don't give a damn! It's over",
+        "Go ahead, make my day... and play tomorrow. Someone has already won."
+    ]
+}
+
+
