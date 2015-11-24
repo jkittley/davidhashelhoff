fef284ae8fba626042836e22a0b87523b01872c4
Mike Jewell <mjewell@gmail.com>
2015-11-24 18:50:36 +0000

    Switching to env

--- createtime.py
+++ createtime.py
@@ -2,13 +2,19 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
 from datetime import datetime
+import os
 
-img = Image.open("/home/david/davidhashelhoff/templates/time.jpg")
+folder = os.environ.get("HASH_HOME")
+if not folder:
+	print "Please set HASH_HOME"
+	sys.exit(0)
+
+img = Image.open(HASH_HOME+"templates/time.jpg")
 
 currently = datetime.now()
 draw = ImageDraw.Draw(img)
-font = ImageFont.truetype("/home/david/davidhashelhoff/fonts/ruben.ttf", 60)
+font = ImageFont.truetype(HASH_HOME+fonts/ruben.ttf", 60)
 
 draw.text((360, 40), currently.strftime("%H:%M:%S"), (0,0,0), font=font)
 draw.text((330, 120), currently.strftime("%d/%m/%Y"), (0,0,0), font=font)
-img.save('/home/david/davidhashelhoff/images/time.jpg')
+img.save(HASH_HOME+'images/time.jpg')
