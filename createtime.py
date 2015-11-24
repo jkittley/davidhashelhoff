26d749df7157196d4afa55ba5183667d37748b6e
Mike Jewell <mjewell@gmail.com>
2015-11-24 18:56:19 +0000

    Fixup of file path

--- createtime.py
+++ createtime.py
@@ -4,17 +4,14 @@ from PIL import ImageDraw
 from datetime import datetime
 import os
 
-folder = os.environ.get("HASH_HOME")
-if not folder:
-	print "Please set HASH_HOME"
-	sys.exit(0)
+HASH_HOME = os.path.dirname(os.path.realpath(__file__))
 
-img = Image.open(HASH_HOME+"templates/time.jpg")
+img = Image.open(HASH_HOME+"/templates/time.jpg")
 
 currently = datetime.now()
 draw = ImageDraw.Draw(img)
-font = ImageFont.truetype(HASH_HOME+"fonts/ruben.ttf", 60)
+font = ImageFont.truetype(HASH_HOME+"/fonts/ruben.ttf", 60)
 
 draw.text((360, 40), currently.strftime("%H:%M:%S"), (0,0,0), font=font)
 draw.text((330, 120), currently.strftime("%d/%m/%Y"), (0,0,0), font=font)
-img.save(HASH_HOME+'images/time.jpg')
+img.save(HASH_HOME+'/images/time.jpg')
