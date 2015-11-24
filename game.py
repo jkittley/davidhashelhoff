d08b77ea1c4745eee8deeb38bee1336dfcafe071
jkittley <jkd3g11@soton.ac.uk>
2015-11-24 14:13:49 +0000

    Added twitter pictures

--- game.py
+++ game.py
@@ -1,11 +1,15 @@
 import tweepy
 from game_secrets import *
 
+# Autheniticate
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+# List timeline contents
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print tweet.text
+
+# Post a status update
+# api.update_status(status="Soon we will be able to play together!");
