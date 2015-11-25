cc6e06e262e9aa4a36a0bfd3125e3deb20121864
Mike Jewell <mjewell@gmail.com>
2015-11-25 17:21:17 +0000

    Working on servo code

--- servo.py
+++ servo.py
@@ -5,16 +5,15 @@ RIGHT = 0.8
 LEFT = 1.6 
 CENTER = 1.2 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-pwm = GPIO.PWM(18, 50)
-pwm.start(0)
-
 def update(ms):
   duty = ((ms/1000.0)*50)*100
   pwm.ChangeDutyCycle(duty)
 
 def ring_bell():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(18, GPIO.OUT)
+  pwm = GPIO.PWM(18, 50)
+  pwm.start(0)
   update(RIGHT)
   time.sleep(2)
   update(LEFT)
@@ -23,8 +22,5 @@ def ring_bell():
   time.sleep(0.5)
   update(CENTER)
   time.sleep(1)
-
-ring_bell()
-
-pwm.stop()
-GPIO.cleanup()
+  pwm.stop()
+  GPIO.cleanup()
